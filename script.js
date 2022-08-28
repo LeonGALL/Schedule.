756b9f754ab4ce5cf2faf5ddff12a5b8f5005d5c
Léon GALL <gallleon194@gmail.com>
2022-08-28 15:29:00 -0400

    Add, modify, and delete

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-// AT THE LOADING OF THE PAGE : 
+// AT THE LOADING OF THE PAGE :
 setMenuWeek();
 setPageWeek();
-
 
 // PAGES and MENU
 var week_link = document.getElementById("week_link");
@@ -60,7 +59,6 @@ function setMenuNutrition() {
   return true;
 }
 
-
 // AJAX REQUESTS TO GET THE ASKED PAGE
 function setPageWeek() {
   return false;
@@ -73,3 +71,83 @@ function setPageDay() {
 function setPageNutrition() {
   return false;
 }
+
+// TASK
+[].forEach.call(document.querySelectorAll(".task h3"), (task) => {
+  task.onclick = (e) => {
+    e.target.parentNode.classList.toggle("done");
+  };
+  task.oncontextmenu = (e) => {
+    oncontextmenu__(e);
+  };
+});
+
+function oncontextmenu__(e) {
+  e.preventDefault();
+  document.getElementById("schedule_container").classList.add("opacity");
+  const rect = e.target.getBoundingClientRect();
+  let menu = document.getElementById("task_right_menu");
+  menu.classList.add("visible");
+  menu.style.top =
+    "" +
+    (rect.top - document.body.scrollTop + e.target.offsetHeight / 3) +
+    "px";
+  menu.style.left = "" + (rect.left - document.body.scrollLeft) + "px";
+  document.body.onclick = (ev) => {
+    ev.preventDefault();
+    menu.classList.remove("visible");
+    document.getElementById("schedule_container").classList.remove("opacity");
+  };
+  document.getElementById("modify").onclick = (ev) => {
+    task_edit(e.target);
+    save();
+  };
+  document.getElementById("delete").onclick = (ev) => {
+    e.target.parentNode.remove();
+    save();
+  };
+}
+
+// Add a task
+function createTask(elem, text) {
+  let div = document.createElement("div");
+  let h3 = document.createElement("h3");
+  div.setAttribute("class", "task");
+  h3.innerText = text;
+  div.appendChild(h3);
+  elem.appendChild(div);
+  return h3;
+}
+
+[].forEach.call(document.querySelectorAll(".plusbutton"), (plus) => {
+  plus.onclick = (e) => {
+    var task = createTask(e.target.parentNode, "");
+    task_edit(task);
+    task.onclick = (e) => {
+      e.target.parentNode.classList.toggle("done");
+    };
+    task.oncontextmenu = oncontextmenu__(e);
+  };
+});
+
+function save() {
+  return false;
+}
+
+function task_edit(elem) {
+  current_elem = elem;
+  var editask = document.getElementById("editask");
+  document.getElementById("schedule_container").classList.add("opacity");
+  editask.classList.add("visible");
+  editask.value = elem.innerText;
+}
+var current_elem;
+document
+  .getElementById("editask")
+  .parentNode.addEventListener("submit", (e) => {
+    e.preventDefault();
+    current_elem.innerText = document.getElementById("editask").value;
+    document.getElementById("schedule_container").classList.remove("opacity");
+    editask.classList.remove("visible");
+    editask.value = "";
+  });
